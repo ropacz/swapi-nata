6e5b6966487524eb73f686b203f52bf0c9a34f15
Rodrigo Paczkovski <paczweb@gmail.com>
2021-04-17 22:36:23 -0300

    feat: create button component

---
+++ src/componets/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  background: #f3db3b;
+  height: 56px;
+  border-radius: 10px;
+  border: 0px;
+  padding: 0 16px;
+  color: #312e38;
+  width: 100%;
+  font-weight: 500;
+  margin-top: 16px;
+  transition: background-color 0.2 s;
+
+  &:hover {
+    background-color: #f3db3b;
+  }
+`;
