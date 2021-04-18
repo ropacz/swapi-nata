1d6dae9656dd00f5e18499c25c51a8d11e104c64
Rodrigo Paczkovski <paczweb@gmail.com>
2021-04-18 15:22:30 -0300

    fix: add links from react router

--- src/componets/Header/index.tsx
+++ src/componets/Header/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { HeaderPage } from './styles';
 
 const Header: React.FC = () => (
   <HeaderPage>
     <div className="container">
-      <h1>Nata Swapi</h1>
+      <h1>
+        <Link to="/">Nata Starships</Link>
+      </h1>
       <nav>
         <ul>
           <li>
-            <p>Home</p>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <p>Sobre</p>
+            <Link to="/about">Sobre</Link>
           </li>
           <li>
-            <p>Github</p>
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/ropacz/swapi-nata"
+              target="_blank"
+            >
+              Github
+            </a>
           </li>
         </ul>
       </nav>
