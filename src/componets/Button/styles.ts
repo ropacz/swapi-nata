a0974133f6a26dbf0a2acb4ba67499a95343809e
Rodrigo Paczkovski <paczweb@gmail.com>
2021-04-18 15:20:04 -0300

    styles: change measurement border-radius

--- src/componets/Button/styles.ts
+++ src/componets/Button/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.button`
   background: #f3db3b;
   height: 56px;
-  border-radius: 10px;
+  border-radius: 0.4em;
   border: 0px;
   padding: 0 16px;
   color: #312e38;
