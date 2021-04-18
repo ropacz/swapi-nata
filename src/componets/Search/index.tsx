6d977d50aae5d05b15d6d15284955ecbefebcad3
Rodrigo Paczkovski <paczweb@gmail.com>
2021-04-18 17:23:55 -0300

    fix: change placeholder text

--- src/componets/Search/index.tsx
+++ src/componets/Search/index.tsx
@@ -56,7 +56,7 @@ const Search: React.FC<SearchProps> = ({ handleDistanceMglt, distance }) => {
           {...register('mglt')}
           defaultValue={distance ?? ''}
           type="text"
-          placeholder="Digite uma medida MGTL (anos luz), ex: 1000000"
+          placeholder="Digite uma distâcia MGTL (anos luz), ex: 1000000"
         />
         <button type="submit">Buscar Naves</button>
       </Form>
