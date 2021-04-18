6be4c97c511d5957d559033c9db5687414502573
Rodrigo Paczkovski <paczweb@gmail.com>
2021-04-17 22:33:06 -0300

    feat: create global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+import starsPattern from '../assets/images/stars-pattern.png';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0
+  }
+
+  body {
+    background: url(${starsPattern}) center;
+    background-color: #323133;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h6, strong {
+    font-weight: 500;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  .container {
+    display: flex;
+    max-width: 1020px;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    flex-wrap: wrap;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+`;
